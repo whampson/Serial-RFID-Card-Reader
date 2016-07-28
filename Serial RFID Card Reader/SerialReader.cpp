8a2eb6e1dc93fed56c8af9d443366361755e6499
Wes Hampson <wesleyhampson@gmail.com>
2016-07-28 14:22:04 -0500

    Replaced tab characters with spaces.

--- Serial RFID Card Reader/SerialReader.cpp
+++ Serial RFID Card Reader/SerialReader.cpp
@@ -17,119 +17,119 @@
 
 HANDLE SerialReader::GetSerialPortHandle(int commPortNumber)
 {
-	// Build COM port name as a wide string (COM[n])
-	wchar_t szCommPortName[5];
-	swprintf(szCommPortName, L"COM%d", commPortNumber);
+    // Build COM port name as a wide string (COM[n])
+    wchar_t szCommPortName[5];
+    swprintf(szCommPortName, L"COM%d", commPortNumber);
 
-	// Get COM port handle
-	HANDLE hComm;
-	hComm = CreateFile(szCommPortName,		// COM port name
-		GENERIC_READ,						// Read data
-		0,									// No sharing, must be opened with exclusive access
-		NULL,								// Default security attributes
-		OPEN_EXISTING,						// Open existing file (required)
-		FILE_FLAG_OVERLAPPED				// Enable overlapped I/O for event-driven reading
-			| FILE_FLAG_NO_BUFFERING,		// No OS buffering; we will handle buffering ourselves
-		NULL);								// Template must be null for COM devices
+    // Get COM port handle
+    HANDLE hComm;
+    hComm = CreateFile(szCommPortName,      // COM port name
+        GENERIC_READ,                       // Read data
+        0,                                  // No sharing, must be opened with exclusive access
+        NULL,                               // Default security attributes
+        OPEN_EXISTING,                      // Open existing file (required)
+        FILE_FLAG_OVERLAPPED                // Enable overlapped I/O for event-driven reading
+            | FILE_FLAG_NO_BUFFERING,       // No OS buffering; we will handle buffering ourselves
+        NULL);                              // Template must be null for COM devices
 
-	return hComm;
+    return hComm;
 }
 
 SerialReader::SerialReader(int commPortNumber)
 {
-	m_commPortNumber = commPortNumber;
-	m_hSerial = INVALID_HANDLE_VALUE;
-	m_isReading = false;
+    m_commPortNumber = commPortNumber;
+    m_hSerial = INVALID_HANDLE_VALUE;
+    m_isReading = false;
 }
 
 bool SerialReader::Initialize(int baudRate, int dataBits, int stopBits, int parity)
 {
-	// Close existing handle (if already open)
-	Close();
+    // Close existing handle (if already open)
+    Close();
 
-	// Get new handle
-	m_hSerial = GetSerialPortHandle(m_commPortNumber);
-	if (m_hSerial == INVALID_HANDLE_VALUE)
-	{
-		return false;
-	}
+    // Get new handle
+    m_hSerial = GetSerialPortHandle(m_commPortNumber);
+    if (m_hSerial == INVALID_HANDLE_VALUE)
+    {
+        return false;
+    }
 
-	// Initialize serial port parameters
-	DCB dcbSerialParams = { 0 };
-	dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
-	dcbSerialParams.BaudRate = baudRate;
-	dcbSerialParams.ByteSize = dataBits;
-	dcbSerialParams.StopBits = stopBits;
-	dcbSerialParams.Parity = parity;
+    // Initialize serial port parameters
+    DCB dcbSerialParams = { 0 };
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    dcbSerialParams.BaudRate = baudRate;
+    dcbSerialParams.ByteSize = dataBits;
+    dcbSerialParams.StopBits = stopBits;
+    dcbSerialParams.Parity = parity;
 
-	// Initialize timeouts
-	COMMTIMEOUTS timeouts = { 0 };
-	timeouts.ReadIntervalTimeout = SERIAL_READ_INTERVAL_TIMEOUT;
-	timeouts.ReadTotalTimeoutConstant = SERIAL_READ_TOTAL_TIMEOUT_CONSTANT;
-	timeouts.ReadTotalTimeoutMultiplier = SERIAL_READ_TOTAL_TIMEOUT_MULTIPLIER;
+    // Initialize timeouts
+    COMMTIMEOUTS timeouts = { 0 };
+    timeouts.ReadIntervalTimeout = SERIAL_READ_INTERVAL_TIMEOUT;
+    timeouts.ReadTotalTimeoutConstant = SERIAL_READ_TOTAL_TIMEOUT_CONSTANT;
+    timeouts.ReadTotalTimeoutMultiplier = SERIAL_READ_TOTAL_TIMEOUT_MULTIPLIER;
 
-	// Write parameters and timeouts to port and return
-	return SetCommState(m_hSerial, &dcbSerialParams)
-		&& SetCommTimeouts(m_hSerial, &timeouts)
-		&& SetCommMask(m_hSerial, EV_RXCHAR);
+    // Write parameters and timeouts to port and return
+    return SetCommState(m_hSerial, &dcbSerialParams)
+        && SetCommTimeouts(m_hSerial, &timeouts)
+        && SetCommMask(m_hSerial, EV_RXCHAR);
 }
 
 void SerialReader::Close()
 {
-	if (m_hSerial != INVALID_HANDLE_VALUE)
-	{
-		CloseHandle(m_hSerial);
-		m_isReading = false;
-	}
+    if (m_hSerial != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(m_hSerial);
+        m_isReading = false;
+    }
 }
 
 void SerialReader::Read(size_t bufSize, void(*OnRead)(char*))
 {
-	// Allocate buffer
-	char* serialBuf = (char*)calloc(bufSize, sizeof(char));
+    // Allocate buffer
+    char* serialBuf = (char*)calloc(bufSize, sizeof(char));
 
-	// Create read event handle and use it for read event
-	HANDLE hReadEvent = CreateEvent(NULL, TRUE, FALSE, L"RxEvent");
-	OVERLAPPED ovRead = { 0 };
-	ovRead.hEvent = hReadEvent;
+    // Create read event handle and use it for read event
+    HANDLE hReadEvent = CreateEvent(NULL, TRUE, FALSE, L"RxEvent");
+    OVERLAPPED ovRead = { 0 };
+    ovRead.hEvent = hReadEvent;
 
-	DWORD dwBytesRead;
+    DWORD dwBytesRead;
 
-	// Read some data!
-	m_isReading = true;
-	while (m_isReading)
-	{
-		// Check for outstanding read event
-		if (HasOverlappedIoCompleted(&ovRead))
-		{
-			// Read data from serial port and store in buffer
-			if (!ReadFile(m_hSerial, serialBuf, bufSize, &dwBytesRead, &ovRead))
-			{
-				DWORD dwError = GetLastError();
-				if (dwError != ERROR_IO_PENDING)
-				{
-					printf("An error has occured. (%d)\n", dwError);
-					break;
-				}
-			}
-		}
+    // Read some data!
+    m_isReading = true;
+    while (m_isReading)
+    {
+        // Check for outstanding read event
+        if (HasOverlappedIoCompleted(&ovRead))
+        {
+            // Read data from serial port and store in buffer
+            if (!ReadFile(m_hSerial, serialBuf, bufSize, &dwBytesRead, &ovRead))
+            {
+                DWORD dwError = GetLastError();
+                if (dwError != ERROR_IO_PENDING)
+                {
+                    printf("An error has occured. (%d)\n", dwError);
+                    break;
+                }
+            }
+        }
 
-		// Continue if buffer is empty
-		if (serialBuf[0] == 0)
-		{
-			continue;
-		}
+        // Continue if buffer is empty
+        if (serialBuf[0] == 0)
+        {
+            continue;
+        }
 
-		// Checks if the overlapped read process has completed
-		if (GetOverlappedResult(m_hSerial, &ovRead, &dwBytesRead, FALSE))
-		{
-			// Do something with data and zero-out the buffer
-			OnRead(serialBuf);
-			memset(serialBuf, 0, sizeof(serialBuf));
-		}
-	}
+        // Checks if the overlapped read process has completed
+        if (GetOverlappedResult(m_hSerial, &ovRead, &dwBytesRead, FALSE))
+        {
+            // Do something with data and zero-out the buffer
+            OnRead(serialBuf);
+            memset(serialBuf, 0, sizeof(serialBuf));
+        }
+    }
 
-	// Cleanup
-	free(serialBuf);
-	CloseHandle(hReadEvent);
+    // Cleanup
+    free(serialBuf);
+    CloseHandle(hReadEvent);
 }
--- Serial RFID Card Reader/main.cpp
+++ Serial RFID Card Reader/main.cpp
@@ -25,11 +25,11 @@ SerialReader* reader = NULL;
 ///</summary>
 void ShowUsage()
 {
-	printf("%s - Reads data from a serial RFID scanner.\n", APP_NAME);
-	printf("\nUsage:\n    %s com_port\n", APP_NAME);
-	printf("\nOptions:\n");
-	printf("    com_port      The COM port number on which to listen (1 - 9).\n");
-	printf("\n\nWritten by Wes Hampson (2016).\n");
+    printf("%s - Reads data from a serial RFID scanner.\n", APP_NAME);
+    printf("\nUsage:\n    %s com_port\n", APP_NAME);
+    printf("\nOptions:\n");
+    printf("    com_port      The COM port number on which to listen (1 - 9).\n");
+    printf("\n\nWritten by Wes Hampson (2016).\n");
 }
 
 /// <summary>
@@ -37,18 +37,18 @@ void ShowUsage()
 /// </summary>
 BOOL WINAPI ConsoleHandler(DWORD dwSignal)
 {
-	switch (dwSignal)
-	{
-	case CTRL_C_EVENT:
-		if (reader != NULL)
-		{
-			printf("CTRL+C detected! Closing...\n");
-			reader->Close();
-			exit(0);
-		}
-		break;
-	}
-	return TRUE;
+    switch (dwSignal)
+    {
+    case CTRL_C_EVENT:
+        if (reader != NULL)
+        {
+            printf("CTRL+C detected! Closing...\n");
+            reader->Close();
+            exit(0);
+        }
+        break;
+    }
+    return TRUE;
 }
 
 /// <summary>
@@ -57,7 +57,7 @@ BOOL WINAPI ConsoleHandler(DWORD dwSignal)
 /// </summary>
 void PrintData(char* data)
 {
-	printf("%s\n", data);
+    printf("%s\n", data);
 }
 
 /// <summary>
@@ -65,44 +65,44 @@ void PrintData(char* data)
 /// </summary>
 int main(int argc, char* argv[])
 {
-	int commPort = -1;
-	int baudRate = 9600;
-	int dataBits = 8;
-	int stopBits = ONESTOPBIT;
-	int parity = NOPARITY;
-	size_t bufSize = 8;
-	bool success;
+    int commPort = -1;
+    int baudRate = 9600;
+    int dataBits = 8;
+    int stopBits = ONESTOPBIT;
+    int parity = NOPARITY;
+    size_t bufSize = 8;
+    bool success;
 
-	// Process command-line arguments
-	if (argc < 2)
-	{
-		ShowUsage();
-		return 0;
-	}
-	else
-	{
-		commPort = atoi(argv[1]);
-	}
+    // Process command-line arguments
+    if (argc < 2)
+    {
+        ShowUsage();
+        return 0;
+    }
+    else
+    {
+        commPort = atoi(argv[1]);
+    }
 
-	// Set CTRL+C handler.
-	success = SetConsoleCtrlHandler(ConsoleHandler, TRUE);
-	if (!success)
-	{
-		printf("Error setting console handler! (%d)", GetLastError());
-		return 1;
-	}
+    // Set CTRL+C handler.
+    success = SetConsoleCtrlHandler(ConsoleHandler, TRUE);
+    if (!success)
+    {
+        printf("Error setting console handler! (%d)", GetLastError());
+        return 1;
+    }
 
-	// Create and initialize serial reader
-	reader = new SerialReader(commPort);
-	success = reader->Initialize(baudRate, dataBits, stopBits, parity);
-	if (!success)
-	{
-		printf("Error opening COM port! (%d)\n", GetLastError());
-		return 1;
-	}
+    // Create and initialize serial reader
+    reader = new SerialReader(commPort);
+    success = reader->Initialize(baudRate, dataBits, stopBits, parity);
+    if (!success)
+    {
+        printf("Error opening COM port! (%d)\n", GetLastError());
+        return 1;
+    }
 
-	// Read data from serial port, pass PrintData() as a callback function 
-	reader->Read(bufSize, PrintData);
+    // Read data from serial port, pass PrintData() as a callback function 
+    reader->Read(bufSize, PrintData);
 
-	return 0;
+    return 0;
 }
